6fc48dccf1f73584b8bf9941d36e264b6bbbf1ba
Michael Carlstrom <rmc170@case.edu>
2022-12-09 13:21:17 -0800

    serial.c works

--- serial.c
+++ serial.c
@@ -1,8 +1,44 @@
-int main(){
-static const int N =  10000000;
+#include <time.h>
+#include <stdio.h>
+#define N 10000000
+// Set up
+static int ones[N];
+static int negativeOnes[N];
+static int zeros[N];
+static int sum[N];
 
-int v1[N];
-int v2[N];
-    // print("sad");
+void setUp(int* arr,int val){
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = val;
+    }
+}
+
+int main()
+{
+    setUp(ones, 1);
+    setUp(negativeOnes, -1);
+    setUp(zeros, 0);
+    
+    clock_t start, end;
+
+    start = clock();
+    for (int i = 0; i < N; i++)
+    {
+        sum[i] = ones[i] + negativeOnes[i];
+    }
+    end = clock();
+    double duration = ((double)end - start) / CLOCKS_PER_SEC;
+    printf("Time taken to execute in seconds : %f\n", duration);
+
+    // Check Summation
+    for (int i = 0; i < N; i++)
+    {
+        if (sum[i] != zeros[i])
+        {
+            printf("Not Summed Correctly");
+        }
+    }
+
     return 0;
 }
